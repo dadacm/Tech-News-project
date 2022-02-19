06faa4a2fc697d26746e043670b1ab589176b1c6
DADA <davimarque2000@gmail.com>
2022-02-19 09:21:58 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,24 @@
+from tech_news.database import search_news
+# from tech_news.scraper import get_tech_news
+
+
+def query(key, value):
+    regex = {"$regex": value, "$options": "i"}
+    query = {key: regex}
+    return query
+
+
+def tupla_title_url(list):
+    data = []
+    for element in list:
+        data.append((element["title"], element["url"]))
+    return data
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_found = search_news(query("title", title))
+    return tupla_title_url(news_found)
 
 
 # Requisito 7
@@ -16,3 +34,8 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# get_tech_news(15)
+# print(search_by_title(
+# "Novo radiotelescópio africano vai ajudar a fotografar"))
